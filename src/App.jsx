9d302bcf52187b7cc39a760b02c0cdfe827a0ad2
Skruphold <hunterco999@gmail.com>
2022-03-05 17:07:49 -0600

    added a active side bar for page shuffling

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
-import Contact from "./components/testimonials/Testimonials"
+import Contact from "./components/testimonials/Testimonials";
+import Menu from "./components/menu/Menu"
 import "./app.scss";
 import { useState } from "react"
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="app">
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
