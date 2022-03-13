8c9707fceb506b7f391bf694afae5863ac0c424f
Skruphold <hunterco999@gmail.com>
2022-03-12 19:15:16 -0600

    added dynamic functionality to my portfolio buttons added a data.js to use later for dynamically displaying photos of website.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,15 +2,39 @@ import "./portfolio.scss"
 import DNBphoto from "../../assets/Dollars-and-bills.png"
 import ACHphoto from "../../assets/ach-game-review.png"
 import ECTphoto from "../../assets/ECT.png"
+import PortfolioList from "../portfolioList/PortfolioList";
+import { useState } from "react";
 
 export default function Portfolio() {
+    const [selected, setSelected] = useState("featured")
+
+    const list = [
+        {
+            id: "featured",
+            title: "Featured",
+        },
+        {
+            id: "web",
+            title: "Web App",
+        },
+        {
+            id: "mobile",
+            title: "Mobile App",
+        },
+    ];
+
     return (
         <div className='portfolio' id="portfolio">
             <h1>Portfolio</h1>
             <ul>
-                <li className="active">Dollars And Bills</li>
-                <li>ACH Game Review</li>
-                <li>Event Covid Tracker</li>
+                {list.map(item=>(
+                    <PortfolioList
+                        title={item.title}
+                        active={selected === item.id}
+                        setSelected={setSelected}
+                        id={item.id}
+                    />
+                ))}
             </ul>
             <div className="container">
                 <div className="item">
---
+++ src/data.js
@@ -0,0 +1,106 @@
+import DNBphoto from "./assets/Dollars-and-bills.png"
+import ACHphoto from "./assets/ach-game-review.png"
+import ECTphoto from "./assets/ECT.png"
+import NoteTakerPhoto from "./assets/note-taker.PNG"
+import JatePhoto from "./assets/jate.PNG"
+import SNApiGif from "./assets/demo.gif"
+
+
+export const featuredPortfolio = [
+    {
+        id: 1,
+        title: "Dollars And Bills",
+        img:
+            {DNBphoto},
+    },
+    {
+        id: 2,
+        title: "ACH Game Review",
+        img:
+            {ACHphoto},
+    },
+    {
+        id: 3,
+        title: "Event Covid Tracker",
+        img:
+        {ECTphoto}
+    },
+    {
+        id: 4,
+        title: "Note Taker",
+        img:
+            {NoteTakerPhoto}
+    },
+    {
+        id: 5,
+        title: "Just another Text Editor",
+        img:
+            {JatePhoto}
+    },
+    {
+        id: 6,
+        title: "Social Network Api",
+        img:
+            {SNApiGif}
+    },
+];
+
+export const webPortfolio = [
+    {
+        id: 1,
+        title: "Dollars And Bills",
+        img:
+            {DNBphoto},
+    },
+    {
+        id: 2,
+        title: "ACH Game Review",
+        img:
+            {ACHphoto},
+    },
+    {
+        id: 3,
+        title: "Event Covid Tracker",
+        img:
+        {ECTphoto}
+    },
+    {
+        id: 4,
+        title: "Note Taker",
+        img:
+            {NoteTakerPhoto}
+    },
+    {
+        id: 5,
+        title: "Just another Text Editor",
+        img:
+            {JatePhoto}
+    },
+];
+
+export const mobilePortfolio = [
+    {
+        id: 1,
+        title: "Dollars And Bills",
+        img:
+            {DNBphoto},
+    },
+    {
+        id: 2,
+        title: "ACH Game Review",
+        img:
+            {ACHphoto},
+    },
+    {
+        id: 3,
+        title: "Event Covid Tracker",
+        img:
+        {ECTphoto}
+    },
+    {
+        id: 4,
+        title: "Note Taker",
+        img:
+            {NoteTakerPhoto}
+    },
+];
