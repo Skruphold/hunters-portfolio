6583b76d9f1e3283784efb8b93402d56326d430c
Skruphold <hunterco999@gmail.com>
2022-03-22 23:36:08 -0500

    added some more easily accessable links to places to contact me as well as added number and email to menu in case visiting on mobile and the navbar doesnt display them.

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,5 +1,8 @@
 import "./menu.scss"
 import Resume from "../../assets/Linkedinresume.pdf"
+import Linkedin from "../../assets/linkedin.png"
+import Twitter from "../../assets/twitter.png"
+import Github from "../../assets/GitHub-Mark-Light-64px.png"
 
 export default function Menu({ menuOpen, setMenuOpen }) {
     return (
@@ -15,11 +18,16 @@ export default function Menu({ menuOpen, setMenuOpen }) {
                     <a href="#works">Works</a>
                 </li>
                 <li onClick={()=>setMenuOpen(false)}>
-                    <a href={Resume}>Resume</a>
-                </li>
-                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#contact">Contact</a>
                 </li>
+                <li onClick={()=>setMenuOpen(false)}>
+                    <a href={Resume}>Resume</a>
+                </li>
+                    <a className="linked-in" href="https://www.linkedin.com/in/hunter-coleman-webdev/"><img src={Linkedin} alt=""/></a>
+                    <a className="twitter" href="https://twitter.com/huntergotvision"><img src={Twitter} alt=""/></a>
+                    <a className="github" href="https://github.com/Skruphold"><img src={Github} alt=""/></a>
+                <p>Hunterco999@gmail.com</p>
+                <p>952-457-8900</p>
             </ul>
         </div>
     )
